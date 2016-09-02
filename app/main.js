ef69906a4630ec96600813ac7bebe3d4426d8f6b
bssthu <bssthu@users.noreply.github.com>
2016-09-03 03:07:41 +0800

    init electron + angular project

---
+++ app/main.js
@@ -0,0 +1,25 @@
+const electron = require('electron');  
+const app = electron.app;  
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        name: "gauto-lesson-planner",
+        width: 800,
+        height: 600,
+        toolbar: false
+    });
+
+    mainWindow.loadURL('file://' + __dirname + "/app/index.html");
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+
+});
