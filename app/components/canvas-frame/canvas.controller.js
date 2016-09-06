544e75b7da8c502ac4284df63d15add57fc8ddca
bssthu <bssthu@users.noreply.github.com>
2016-09-07 00:04:34 +0800

    draw step rect

--- app/components/canvas-frame/canvas.controller.js
+++ app/components/canvas-frame/canvas.controller.js
@@ -14,15 +14,48 @@ function CanvasFrameCtrl($scope, CanvasService, DbService) {
             return;
         }
         CanvasService.updateCurrentRecords($scope.currentRecords);
-        draw();
+        draw(context, $scope.nodes);
     }, true);
+
+    draw(context, $scope.nodes);
 
     /**
      * 绘图函数
+     * @param context
+     * @param nodes 出现过的 step id，升序排列
      */
-    function draw() {
-        console.log($scope.nodes);
+    function draw(context, nodes) {
+        context.canvas.width = window.innerWidth;
+        context.canvas.height = window.innerHeight;
+
+        for (var i = 0; i < nodes.length; i++) {
+            drawStep(context, i, nodes.length, nodes[i]);
+        }
     }
 
-    draw();
+    /**
+     *
+     * @param context
+     * @param id 图形从上到下的编号
+     * @param idNum id 个数
+     * @param stepId step 的编号
+     */
+    function drawStep(context, id, idNum, stepId) {
+        var windowWidth = context.canvas.width;
+        var windowHeight = context.canvas.height;
+        var cx = windowWidth / 2;
+        var cy = windowHeight / (idNum + 1) * (id + 1);
+        // custom
+        var width = windowWidth / 5;
+        var height = windowHeight / idNum / 2;
+        // rect
+        context.rect(cx - width / 2, cy - height / 2, width, height);
+        context.stroke();
+        // text
+        var fontArgs = context.font.split(' ');
+        context.textAlign = 'center';
+        context.font = '20px ' + fontArgs[fontArgs.length - 1];
+        console.log(context.font);
+        context.fillText('step_' + stepId, cx, cy);
+    }
 }
